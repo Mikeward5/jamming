d7d3e0497c8c54fa6f119f48c2f68ff47df91e08
Mike <mikeward5@hotmail.co.uk>
2024-09-18 16:33:46 +0100

    daily end commit

--- src/components/PlayLists/PlayList.js
+++ src/components/PlayLists/PlayList.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './PlayList.css';
+import Track from '../Track/Track';
 
 function PlayList() {
+
+
 
     return (
         <>
         <div className='PlayList'>
-            <h1>Search Results</h1>
-            <div className='PlayInnerbox'>
-            <h2>Written word</h2>
-        </div>
+            <h1>PlayList</h1>
+
+            <Track />
+            
         </div>
         </>
     )
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -3,11 +3,16 @@ import './Track.css';
 
 
 function Track(props) {
+
     return (
         <div className="TrackInnerbox">
             <h3>{props.name}</h3>
             <p>{props.album} | {props.artist}</p>
+            <div>
+                <button className="button" onClick={props.handleClick}> + </button>
+            </div>
         </div>
+        
     );
 };
 
